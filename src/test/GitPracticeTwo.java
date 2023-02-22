bd6a6cb74e2b99c0b3877a5c491ca9cecbf8a9c1
ozgee <ozge.erol@tekschool.us>
2023-02-22 11:02:25 -0500

    this is for another pushing practice

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -4,7 +4,7 @@ public class GitPracticeTwo {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("This is for Push practice");
+	
 		
 		System.out.println("ozge");
 		System.out.println("this is for trial");
