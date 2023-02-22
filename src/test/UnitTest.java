b82438878b05feb0139329f9761a258b66941d01
ozgee <ozge.erol@tekschool.us>
2023-02-21 19:43:23 -0500

    new method added and old one deleted

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -4,12 +4,6 @@ public class UnitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int a=10;
-		int b=15;
-		int c = a+b;
-		
-		System.out.println(c);
 
 	}
 
