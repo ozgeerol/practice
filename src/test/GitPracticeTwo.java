945b893426b58ef93d6b465fc62c6d00602e2120
ozgee <ozge.erol@tekschool.us>
2023-02-22 00:13:48 -0500

    new method added and old one deleted

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -1,5 +1,10 @@
 package test;
 
 public class GitPracticeTwo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is for Push practice");
+	}
 
 }
--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -4,6 +4,13 @@ public class UnitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		System.out.println(testOne(10,5));
+		}
+	public static int testOne(int a, int b) {
+		return a-b;
+	
 
 	}
 
